07aa6fca2538cadfa1768113e9c31dd64cac341e
Timothy Shtunyuk <timshtu@gmail.com>
2025-08-30 18:16:19 -0500

    Task 1. Add who is saying 'hello'.

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,6 @@
 class HelloWorldApp {
  public static void main(String[] args) {
  System.out.println("Hello World!"); // Display the string.
+ System.out.println(“Says George.”); // Task 1 – Display who is saying ‘hello’.
  }
 }
