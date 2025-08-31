010c020ff511869dd767c4fa74ee2d72f6d035bd
Timothy Shtunyuk <timshtu@gmail.com>
2025-08-30 19:51:52 -0500

    Manual merge with Task 1 changes.

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,11 +1,6 @@
 class HelloWorldApp {
  public static void main(String[] args) {
-<<<<<<< HEAD
  System.out.println("Hello Universe!"); // Task 2. Be more inclusive
  System.out.println("From Earth."); // Task 2. Be more inclusive
-=======
- System.out.println("Hello World!"); // Display the string.
- System.out.println(“Says George.”); // Task 1 – Display who is saying ‘hello’.
->>>>>>> 86479c9651989796b36f5ae98371b373776cf08b
  }
 }
