432923817d7b5111c79e7ecbda8136f83973a5de
Timothy Shtunyuk <timshtu@gmail.com>
2025-08-30 19:42:34 -0500

    Task 2. Changes to be more inclusive

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,6 +1,6 @@
 class HelloWorldApp {
  public static void main(String[] args) {
- System.out.println("Hello Universe!"); // Task 2. Be more inclusive.
- System.out.println("From Earth."); // Task 2. Be more inclusive.
+ System.out.println("Hello Universe!"); // Task 2. Be more inclusive
+ System.out.println("From Earth."); // Task 2. Be more inclusive
  }
 }
